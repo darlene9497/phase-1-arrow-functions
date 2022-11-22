3558b9ddb3f12a7340109c13c8810006e85e954c
darlene9497 <darlene.nandabi@student.moringaschool.com>
2022-11-22 11:51:45 -0500

    Complete assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(a,b){
+    a=2000
+    b=100
+    return a/b
+};
+const square = (a) => {
+    a=2
+    return a*a
+};
+const add = (a,b) => {
+    a=3
+    b=4
+    return a + b
+};
